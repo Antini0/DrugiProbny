6e2aaa3fbcf8480b5cbfc718d8cfe0fdc8f1af96
Antoni Dawiowicz <s27366@pjwstk.edu.pl>
2024-06-08 14:02:33 +0200

    asd

---
+++ DrugiProbny/Models/Client.cs
@@ -0,0 +1,13 @@
+﻿namespace DrugiProbny.Models;
+
+public class Client
+{
+    [Key]
+    public int Id { get; set; }
+    [MaxLength(100)]
+    public string FirstName { get; set; } = string.Empty;
+    [MaxLength(120)]
+    public string LastName { get; set; } = string.Empty;
+
+    public ICollection<Order> Orders { get; set; } = new HashSet<Order>();
+}
---
+++ DrugiProbny/Services/IDbService.cs
@@ -0,0 +1,12 @@
+﻿namespace DrugiProbny.Services;
+
+
+public interface IDbService
+{
+    Task<ICollection<Order>> GetOrdersData(string? clientLastName);
+    Task<bool> DoesClientExist(int clientID);
+    Task<bool> DoesEmployeeExist(int employeeID);
+    Task AddNewOrder(Order order);
+    Task<Pastry?> GetPastryByName(string name);
+    Task AddOrderPastries(IEnumerable<OrderPastry> orderPastries);
+}
